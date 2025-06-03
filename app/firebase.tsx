aa2219c41843e404d2c88ee7f3b693c0754d7049
Ronan <ronanscampos@hotmail.com>
2025-06-03 16:59:37 -0400

    Protect data sensive

--- app/firebase.tsx
+++ app/firebase.tsx
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXG3ueiSzveXS9SLjgNzVEAPhySoJ405Q",
-  authDomain: "gamersconnections.firebaseapp.com",
-  projectId: "gamersconnections",
-  storageBucket: "gamersconnections.appspot.com", 
-  messagingSenderId: "490603957656",
-  appId: "1:490603957656:web:a99c25a380c7027c4bafed",
-  databaseURL: "https://gamersconnections-default-rtdb.firebaseio.com",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "", 
+  messagingSenderId: "",
+  appId: "",
+  databaseURL: "",
 };
 
 const app = initializeApp(firebaseConfig);
